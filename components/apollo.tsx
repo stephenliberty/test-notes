552dc89ba9cf1851278109083b60bde484cc6b47
sliberty <sliberty@cvent.com>
2023-08-12 16:42:40 -0400

    Just hardcode the value for the moment

--- components/apollo.tsx
+++ components/apollo.tsx
@@ -10,7 +10,7 @@ import React from "react";
 
 function makeClient() {
   const httpLink = new HttpLink({
-    uri: "http://localhost:3000/api/graphql",
+    uri: "https://test-notes-af744cef564a.herokuapp.com/",
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     fetchOptions: { cache: "no-store" },
